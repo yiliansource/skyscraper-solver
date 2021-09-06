52a36491265843c2bae120183c3119b3628e413f
yiliansource <yiliansource@gmail.com>
2021-09-06 15:58:38 +0000

    Deploying to gh-pages from @ yiliansource/skyscraper-solver@8f8b7e170f2f3a5faad0d1f8f935d315bf4b5e47 🚀

---
+++ _next/static/U8del6qvku_aKOoP3Gbds/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/542-b55d6616d9177f46d38b.js","static/chunks/pages/index-01a48222d4bab9fa8502.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
